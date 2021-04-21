86eda0d4488d041991a23e05091588dff397f457
Theo-Steiner <theosteiner@gmail.com>
2021-04-21 17:46:54 +0900

    Removed transition, now instantly insert image when loaded

--- src/lib/UI/Image.svelte
+++ src/lib/UI/Image.svelte
@@ -1,42 +1,41 @@
 <script>
-    export let srcset;
-    export let src;
-    export let alt;
-    export let sizes;
+	export let srcset;
+	export let src;
+	export let alt;
+	export let sizes;
 
-    import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
-    let loaded = false;
-    let thisImage;
+	let loaded = false;
+	let thisImage;
 
-    onMount(() => {
-        thisImage.onload = () => {
-            loaded = true;
-        };
-    });
+	onMount(() => {
+		thisImage.onload = () => {
+			loaded = true;
+		};
+	});
 </script>
 
 <img
-    {sizes}
-    {srcset}
-    {alt}
-    {src}
-    class:loaded
-    bind:this={thisImage}
-    draggable="false"
-    loading="lazy"
+	{sizes}
+	{srcset}
+	{alt}
+	{src}
+	class:loaded
+	bind:this={thisImage}
+	draggable="false"
+	loading="lazy"
 />
 
 <style>
-    img {
-        width: 100%;
-        height: 100%;
-        display: block;
-        opacity: 0;
-        transition: opacity 1000ms ease-out;
-        object-fit: cover;
-    }
-    img.loaded {
-        opacity: 1;
-    }
+	img {
+		width: 100%;
+		height: 100%;
+		display: block;
+		opacity: 0;
+		object-fit: cover;
+	}
+	img.loaded {
+		opacity: 1;
+	}
 </style>
