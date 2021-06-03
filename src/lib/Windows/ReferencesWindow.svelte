52c2248b54ab4e7c393ae4bb24d55d03ab3adb2d
Theo Steiner <theosteiner@gmail.com>
2021-06-03 17:32:12 +0900

    Update: Reference Window now has vertical infinite marquee

--- src/lib/Windows/ReferencesWindow.svelte
+++ src/lib/Windows/ReferencesWindow.svelte
@@ -4,34 +4,32 @@
 
 <div class="grid-area">
 	<WindowElement
-		width={347}
-		height={314}
+		width={376}
+		height={408}
 		parallax="medium"
-		background="#5F583D"
-		title="References"
+		background="#151515"
+		title="REFERENCES"
 		enlargeable={false}
 		id={4}
 		isInForeground={true}
 	>
-		<ul>
-			<li><a href="/">Bureau Johannes Erler</a></li>
-			<li><a href="/">CarlNann</a></li>
-			<li><a href="/">Con Corazón</a></li>
-			<li>
-				<a href="/">Düsseldorfer Schauspielhaus</a>
-			</li>
-			<li><a href="/">Eberhard Müller Consulting</a></li>
-			<li><a href="/">Fargo</a></li>
-			<li><a href="/">Konica Minolta</a></li>
-			<li><a href="/">Morgenbladet</a></li>
-			<li><a href="/">Otto Group</a></li>
-			<li>
-				<a href="/">Staatsschauspielhaus Dresden</a>
-			</li>
-			<li><a href="/">Steiner Am Fluss</a></li>
-			<li><a href="/">Suhrkamp</a></li>
-			<li><a href="/">Zoeva</a></li>
-		</ul>
+		<div class="pause-animation">
+			<ul style="--th: 390px;">
+				<li>Bureau Johannes Erler</li>
+				<li>CarlNann</li>
+				<li>Con Corazón</li>
+				<li>Düsseldorfer Schauspielhaus</li>
+				<li>Eberhard Müller Consulting</li>
+				<li>Fargo</li>
+				<li>Konica Minolta</li>
+				<li>Morgenbladet</li>
+				<li>Otto Group</li>
+				<li>Staatsschauspielhaus Dresden</li>
+				<li>Steiner Am Fluss</li>
+				<li>Suhrkamp</li>
+				<li>Zoeva</li>
+			</ul>
+		</div>
 	</WindowElement>
 </div>
 
@@ -41,25 +39,41 @@
 		grid-row: 116/142;
 		display: flex;
 		justify-content: center;
-		align-items: center;
 	}
-	a {
-		color: #151515;
-		font-size: 15px;
-		margin: 0px;
+
+	li {
+		vertical-align: middle;
+		text-align: center;
+		padding: 6px 0;
+		color: #fefefe;
+		font-size: 14px;
+		line-height: 18px;
+		letter-spacing: -0.17px;
+		text-transform: uppercase;
 		text-decoration: none;
-	}
-	a:hover {
-		text-decoration: underline #a25c24;
 	}
 
 	ul {
 		list-style-type: none;
-		padding: 10px 0px 13px 11px;
-		margin: 0;
+		margin: 0px;
+		text-shadow: 0 var(--th) #fefefe, 0 calc(var(--th) * 2) #fefefe, 0 calc(var(--th) * 3) #fefefe,
+			0 calc(var(--th) * 4) #fefefe;
+		will-change: transform;
+		animation: marquee 10s linear infinite;
+		animation-play-state: running;
 	}
-	li {
-		padding-top: 1px;
+
+	.pause-animation:hover ul {
+		animation-play-state: paused;
+	}
+
+	@keyframes marquee {
+		0% {
+			transform: translateY(0);
+		}
+		100% {
+			transform: translateY(-100%);
+		}
 	}
 
 	@media only screen and (min-width: 1440px) {
