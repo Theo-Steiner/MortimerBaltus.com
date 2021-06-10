e824591eb9b29fc482418fcd79cfc16e5afd895b
Theo Steiner <theosteiner@gmail.com>
2021-06-10 15:47:33 +0900

    Add: con corazon subpage, with not-yet responsive image sizes and no scroll animations

--- src/routes/pages/project_01.svelte
+++ src/routes/pages/project_01.svelte
@@ -25,14 +25,47 @@
 	</p>
 	<a href="https://concorazon-hamburg.com/">concorazon-hamburg.com ↗</a>
 
-	<div
-		class="background-image"
-		style="background-image: url({`${slug}con_corazon_necklace_ef6uzk.jpg`});"
-	>
-		<div class="visitenkarten">
+	<div class="visitenkarten-container">
+		<img
+			class="vk-image"
+			src={`${slug}con_corazon_necklace_ef6uzk.jpg`}
+			alt="Beautiful chain dangling from a woman's neck"
+		/>
+		<div class="visitenkarten stack">
 			<img class="vk" src={`${slug}con_corazon_vk_ghziwc`} alt="Vorderseite der Visitenkarte" />
-			<img class="vk" src={`${slug}con_corazon_vk2_qfwien`} alt="Vorderseite der Visitenkarte" />
+			<img class="vk" src={`${slug}con_corazon_vk2_qfwien`} alt="Rückseite der Visitenkarte" />
 		</div>
+	</div>
+	<div class="two-cards">
+		<img
+			class="half-image"
+			src={`${slug}con_corazon_ear_dekidu`}
+			alt="Golden earrings with a blue gem"
+		/>
+		<img class="half-image" src={`${slug}con_corazon_iPhone_01_zz1cvk`} alt="iPhone App design" />
+	</div>
+	<img
+		class="hero-image"
+		src={`${slug}con_corazon_banner_iungvl`}
+		alt="Banner design for Con Corazón"
+	/>
+	<img
+		class="hero-image"
+		src={`${slug}con_corazon_artisan_urpfhn`}
+		alt="An artisan working with hemp strings"
+	/>
+	<img class="hero-image" src={`${slug}con_corazon_emblem_p70r28`} alt="Emblem for Con Corazón" />
+	<div class="two-cards">
+		<img
+			class="half-image"
+			src={`${slug}con_corazon_iPhone_02_w0mhgy`}
+			alt="The shop application designed for Con Corazón"
+		/>
+		<img
+			class="half-image"
+			src={`${slug}con-corazon_craftsman_m0ssbe`}
+			alt="A craftsman focused on his pottery work"
+		/>
 	</div>
 </div>
 
@@ -55,30 +88,56 @@
 		object-position: center bottom;
 	}
 
-	.background-image {
+	.vk-image {
 		width: 100vw;
-		height: 853px;
-		display: flex;
-		background-size: cover;
-		justify-content: center;
-		align-items: flex-end;
+		height: 180vw;
+		max-height: 853px;
+		object-fit: cover;
+		object-position: center top;
+	}
+
+	.visitenkarten-container {
+		position: relative;
+		overflow: hidden;
+		min-height: 400px;
+	}
+
+	.stack {
+		position: absolute;
+		bottom: 0;
+		left: 0;
 	}
 
 	.visitenkarten {
+		z-index: 1;
 		width: 100%;
-		height: 50%;
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: center;
+		padding-bottom: 20vw;
 	}
 
 	.vk {
+		width: 90vw;
+		height: 51vw;
 		max-width: 431px;
-		height: calc(50% - 3.6vw);
 		max-height: 244px;
 		margin: 1.8vw;
 		box-shadow: 0px 15px 25px -20px rgba(82, 102, 104, 0.66),
 			0px 15px 40px -10px rgba(82, 102, 104, 0.33);
+	}
+
+	.two-cards {
+		width: 100vw;
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.half-image {
+		width: 100%;
+		height: 70vh;
+		max-height: 853px;
+		object-fit: cover;
 	}
 
 	p {
@@ -89,10 +148,33 @@
 		display: inline-block;
 		margin: 0px 20px 30px 20px;
 		color: #151515;
-		font-size: 13px;
+		font-size: min(1vw, 13px);
 		line-height: 17.5px;
 		letter-spacing: 0.11px;
 		text-decoration: none;
 		text-align: center;
+	}
+
+	@media only screen and (min-device-width: 768px) {
+		.vk-image {
+			width: 100vw;
+			height: auto;
+		}
+
+		.visitenkarten {
+			padding-bottom: min(10vh, 182px);
+		}
+
+		.vk {
+			width: 30vw;
+			height: 17vw;
+			max-width: none;
+			max-height: none;
+		}
+
+		.half-image {
+			width: 50%;
+			height: auto;
+		}
 	}
 </style>
