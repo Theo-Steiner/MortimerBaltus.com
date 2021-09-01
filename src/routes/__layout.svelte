5a43c855f1c0c850007d1c2595c6374f9497bada
Theo Steiner <theosteiner@gmail.com>
2021-09-01 15:30:21 +0900

    Move PageTransition to index.svelte as having it in the layout prevents onDestroy from being called

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,5 @@
 <script>
-	import PageTransition from '$lib/UX/PageTransition.svelte';
-	import { setContext } from 'svelte';
-
 	import '../app.css';
-
-	let innerHeight;
-	$: setContext('innerHeight', innerHeight);
 </script>
 
-<svelte:window bind:innerHeight />
-<PageTransition {innerHeight}>
-	<slot />
-</PageTransition>
+<slot />
