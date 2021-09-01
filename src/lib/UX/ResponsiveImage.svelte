fe7b1957a8eb07af723a5804b30c091c54fbd932
Theo Steiner <theosteiner@gmail.com>
2021-09-01 18:40:28 +0900

    Make aspect ratio component prop & pick more aggressive quality algorithm

--- src/lib/UX/ResponsiveImage.svelte
+++ src/lib/UX/ResponsiveImage.svelte
@@ -5,11 +5,10 @@
 	export let imageHeight;
 	export let displayWidth = '100vw';
 	export let limitHeight = true;
-	const slug = 'https://res.cloudinary.com/thdrstnr/image/upload';
+	export let aspectRatio = limitHeight ? '32/19' : 'initial';
+	const slug = 'https://res.cloudinary.com/thdrstnr/image/upload/q_auto:best,';
 
-	const srcset = `${slug}/MortimerBaltus/${imageID} ${imageWidth}w, ${slug}/w_1600/MortimerBaltus/${imageID} 1600w, ${slug}/w_1366/MortimerBaltus/${imageID} 1366w, ${slug}/w_1024/MortimerBaltus/${imageID} 1024w, ${slug}/w_768/MortimerBaltus/${imageID} 768w, ${slug}/w_640/MortimerBaltus/${imageID} 640w, ${slug}/w_320/MortimerBaltus/${imageID} 320w, ${slug}/w_160/MortimerBaltus/${imageID} 160w`;
-
-	let aspectRatio = limitHeight ? '32/19' : 'initial';
+	const srcset = `${slug}MortimerBaltus/${imageID} ${imageWidth}w, ${slug}w_1600/MortimerBaltus/${imageID} 1600w, ${slug}w_1366/MortimerBaltus/${imageID} 1366w, ${slug}w_1024/MortimerBaltus/${imageID} 1024w, ${slug}w_768/MortimerBaltus/${imageID} 768w, ${slug}w_640/MortimerBaltus/${imageID} 640w, ${slug}w_320/MortimerBaltus/${imageID} 320w, ${slug}w_160/MortimerBaltus/${imageID} 160w`;
 </script>
 
 <img
