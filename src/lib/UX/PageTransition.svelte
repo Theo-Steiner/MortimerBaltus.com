a0564c6f7a3a764e96b520cba83da659a2aa3077
Theo Steiner <theosteiner@gmail.com>
2021-09-01 14:10:59 +0900

    Rewrite PageTransition to use native CSS transition

--- src/lib/UX/PageTransition.svelte
+++ src/lib/UX/PageTransition.svelte
@@ -1,14 +1,39 @@
 <script>
-	import { slide } from 'svelte/transition';
+	export let innerHeight;
 
-	export let inDelay = 600;
-	export let outDelay = 0;
-	import { quartInOut } from 'svelte/easing';
+	function wait(_, { duration = 400 }) {
+		return {
+			duration
+		};
+	}
+
+	let isTransitioning = false;
 </script>
 
-<div
-	in:slide={{ delay: inDelay, easing: quartInOut }}
-	out:slide={{ delay: outDelay, easing: quartInOut }}
+<main
+	out:wait
+	on:outrostart={() => (isTransitioning = true)}
+	class:is-transitioning={isTransitioning}
+	style="--windowInnerHeight: {innerHeight}px;"
 >
 	<slot />
-</div>
+</main>
+
+<style>
+	main {
+		height: 100vh;
+		height: var(--windowInnerHeight);
+		opacity: 1;
+		width: 100%;
+		backface-visibility: hidden;
+		-webkit-backface-visibility: hidden;
+		-moz-backface-visibility: hidden;
+		transition: all 400ms cubic-bezier(0.76, 0, 0.24, 1);
+		transition-delay: 600ms;
+	}
+
+	.is-transitioning {
+		height: 0px;
+		opacity: 0;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,14 @@
 <script>
 	import PageTransition from '$lib/UX/PageTransition.svelte';
+	import { setContext } from 'svelte';
 
 	import '../app.css';
+
+	let innerHeight;
+	$: setContext('innerHeight', innerHeight);
 </script>
 
-<PageTransition inDelay={400} outDelay={600}>
+<svelte:window bind:innerHeight />
+<PageTransition {innerHeight}>
 	<slot />
 </PageTransition>
