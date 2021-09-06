f3dadb1ef5efeda6a8f7f079a0fe2b7c41b03ab1
Theo Steiner <theosteiner@gmail.com>
2021-09-06 14:31:20 +0900

    Make srcset reactive for gallery to work in local env

--- src/lib/UX/ResponsiveImage.svelte
+++ src/lib/UX/ResponsiveImage.svelte
@@ -8,18 +8,20 @@
 	export let aspectRatio = limitHeight ? '32/19' : 'initial';
 	const slug = 'https://res.cloudinary.com/thdrstnr/image/upload/q_auto:best';
 
-	const srcset = `${slug}/MortimerBaltus/${imageID} ${imageWidth}w, ${slug},w_1600/MortimerBaltus/${imageID} 1600w, ${slug},w_1366/MortimerBaltus/${imageID} 1366w, ${slug},w_1024/MortimerBaltus/${imageID} 1024w, ${slug},w_768/MortimerBaltus/${imageID} 768w, ${slug},w_640/MortimerBaltus/${imageID} 640w, ${slug},w_320/MortimerBaltus/${imageID} 320w, ${slug},w_160/MortimerBaltus/${imageID} 160w`;
+	$: srcset = `${slug}/MortimerBaltus/${imageID} ${imageWidth}w, ${slug},w_1600/MortimerBaltus/${imageID} 1600w, ${slug},w_1366/MortimerBaltus/${imageID} 1366w, ${slug},w_1024/MortimerBaltus/${imageID} 1024w, ${slug},w_768/MortimerBaltus/${imageID} 768w, ${slug},w_640/MortimerBaltus/${imageID} 640w, ${slug},w_320/MortimerBaltus/${imageID} 320w, ${slug},w_160/MortimerBaltus/${imageID} 160w`;
 </script>
 
-<img
-	src={`${slug}/MortimerBaltus/${imageID}`}
-	width={imageWidth}
-	height={imageHeight}
-	sizes={displayWidth}
-	{srcset}
-	{alt}
-	style="--displayWidth: {displayWidth}; aspect-ratio: {aspectRatio};"
-/>
+<section>
+	<img
+		src={`${slug}/MortimerBaltus/${imageID}`}
+		width={imageWidth}
+		height={imageHeight}
+		sizes={displayWidth}
+		{srcset}
+		{alt}
+		style="--displayWidth: {displayWidth}; aspect-ratio: {aspectRatio};"
+	/>
+</section>
 
 <style>
 	img {
@@ -27,5 +29,11 @@
 		height: auto;
 		object-fit: cover;
 		object-position: center bottom;
+	}
+
+	section {
+		display: flex;
+		align-items: center;
+		height: 100vh;
 	}
 </style>
