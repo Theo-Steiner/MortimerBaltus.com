5c8a5baa498ad5aa8994da4e5d0a8766cafb8fa6
Theo Steiner <theosteiner@gmail.com>
2021-09-06 18:59:05 +0900

    Initialize i18n store with locales in uppermost layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,18 @@
 <script>
 	import '../app.css';
+	import {
+		addMessages,
+		init,
+		getLocaleFromNavigator /*, register */
+	} from 'svelte-intl-precompile';
+	import en from '../../locales/en.js';
+	import ja from '../../locales/ja.js';
+	addMessages('en', en);
+	addMessages('ja', ja);
+	init({
+		fallbackLocale: 'en',
+		initialLocale: getLocaleFromNavigator()
+	});
 </script>
 
 <slot />
