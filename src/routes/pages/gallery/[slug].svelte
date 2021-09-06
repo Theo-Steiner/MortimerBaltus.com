e021cebdd5255a4fa49388b66e8b5794e1635c48
Theo Steiner <theosteiner@gmail.com>
2021-09-06 14:33:07 +0900

    Add gallery/[slug].svelte route

---
+++ src/routes/pages/gallery/[slug].svelte
@@ -0,0 +1,25 @@
+<script context="module">
+	import deGallery from '$lib/data/deGallery.json';
+	import jpGallery from '$lib/data/jpGallery.json';
+	export async function load({ page }) {
+		let slug = page.params.slug;
+		let image =
+			deGallery.find((el) => el.name === slug) || jpGallery.find((el) => el.name === slug);
+		return {
+			props: { image }
+		};
+	}
+</script>
+
+<script>
+	import ResponsiveImage from '$lib/UX/ResponsiveImage.svelte';
+	export let image;
+</script>
+
+<ResponsiveImage
+	limitHeight={false}
+	imageHeight={image.height}
+	imageWidth={image.width}
+	imageID={image.src}
+	alt={image.alt}
+/>
