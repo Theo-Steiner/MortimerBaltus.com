c141b73b3b7b674dde303a9ae6519f9684ee024c
Theo Steiner <theosteiner@gmail.com>
2021-09-07 00:35:13 +0900

    Update locale messages (Not yet checked by mone)

--- locales/en.js
+++ locales/en.js
@@ -1,3 +1,50 @@
 export default {
-	about: 'ABOUT'
+	windows: {
+		about: 'ABOUT',
+		contact: 'CONTACT',
+		cookies: 'COOKIES',
+		'clean-code': 'CLEAN.CODE',
+		language: 'LANGUAGE',
+		legal: 'LEGAL NOTICE',
+		privacy: 'PRIVACY POLICY',
+		references: 'REFERENCES',
+		project_01: 'PROJECT_01',
+		project_02: 'PROJECT_02',
+		project_03: 'PROJECT_03',
+		project_04: 'PROJECT_04'
+	},
+	pages: {
+		about: {
+			title: 'ABOUT',
+			introduction:
+				'MortimerBaltus is a 2021 founded partnership between art director Moritz Müller and web developer Theodor Steiner with the purpose of realizing holistic brand experiences by utilizing knowledge and techniques from different fields of profession. \nWe work individually or as a team for agencies, companies and start up businesses. For our clients we aspire new concepts and favour the unconventional over the trend. This way we find individual solutions that can make lasting impressions in an otherwise boringly standardized world.',
+			location:
+				'Located in Hamburg and Tokyo we are able to work closely with our partners from both markets and can provide intercultural guidance if needed. We are always looking forward to get to know new people – and Yes, we also speak Japanese'
+		},
+		gallery: { title: 'GALLERY' },
+		// LEGAL NOT TRANSLATED FOR NOW
+		legal: { title: 'LEGAL NOTICE' },
+		// PRIVACY NOT TRANSLATED FOR NOW
+		privacy: { title: 'PRIVACY POLICY' },
+		project_01: {
+			title: 'PROJECT_01',
+			introduction:
+				'Con Corazón is a brand supporting artisans from countries at war by embracing their cultural heritage and distributing the unique goods through an appealing online shop. In close coordination with the team at Con Corazón a corporate design and its core values have been created and defined to give the artisan goods a competitive visual identity in the world of contemporary online shopping.'
+		},
+		project_02: {
+			title: 'PROJECT_02',
+			introduction:
+				'PRODUKT is a multi-area brand selling everything from streetwear fashion to lifestyle goods and accessories. For the launch of the webshop, we defined a brand identity, designed logo and corporate guidelines, helped with the design and packaging of the products, organized and finalized photoshoots and developed the online store itself. The brand aims to grow their product range continuously. Therefore, we selected an easily manageable cms and designed the shopping-experience for the customer accordingly.'
+		},
+		project_03: {
+			title: 'PROJECT_03',
+			introduction:
+				'Eberhard Müller develops sophis­ti­cated interiors at the highest level. For this, the small agency from Lake Starnberg near Munich needed a corporate design fitting their high standards across all media platforms.'
+		},
+		project_04: {
+			title: 'PROJECT_04',
+			introduction:
+				"This design proposal was developed as part of the visual identity development for a perfume start-up located in Germany. As part of this process we visualized multiple concepts for the client to decide which overall look suited their wishes and requirements best.\nDuring a very successfull launch in 2021 the perfume's first edition sold out in just 2 weeks - starting off the start-up finally branded as Soèm perfectly.\n\nThe process of brand identity development, which makes our work so enjoyable and special usually leaves us with great results that remain unseen. Therefore, we decided to use this space to showcase what is left on the cutting room floor."
+		}
+	}
 };
