a27a769fd91d626da829a3a4d2561e1cfc76eeb3
Theo Steiner <theosteiner@gmail.com>
2021-11-13 18:24:21 +0900

    Add fallthrough error page/ fix error bug

---
+++ src/routes/__error.svelte
@@ -0,0 +1,59 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				title: status === 404 ? '404: Page not found' : `${status} ${error.message}`
+			}
+		};
+	}
+</script>
+
+<script>
+	import Footer from '$lib/UI/Footer.svelte';
+	import Navigation from '$lib/UI/Navigation.svelte';
+	import SubpageTransition from '$lib/UX/SubpageTransition.svelte';
+	import { t } from 'precompile-intl-runtime';
+
+	export let title;
+</script>
+
+<SubpageTransition>
+	<Navigation previousLink="/" innerWidth nextLink="/pages/about" currentPage="error" />
+	<div>
+		<section>
+			<h1 lang="en">{title}</h1>
+			<p>{$t('pages.error.errorMessage')}</p>
+		</section>
+		<Footer currentPage="error" />
+	</div>
+</SubpageTransition>
+
+<style>
+	div {
+		overflow: scroll;
+		height: 100%;
+	}
+
+	section {
+		height: 100vh;
+		background: #e63c54;
+		flex-grow: 1;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 53px 5vw;
+		color: #fefefe;
+		text-align: center;
+	}
+
+	h1 {
+		font-size: clamp(40px, 6.7vw, 80px);
+		padding-top: 53px;
+	}
+
+	p {
+		color: inherit;
+		max-width: 63ch;
+	}
+</style>
